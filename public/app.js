09ddd043c5296004eff763fdc70510474506578b
Keithkiarie <keithkiarie@gmail.com>
2021-03-13 14:33:03 +0300

    load models

--- public/app.js
+++ public/app.js
@@ -1,8 +1,21 @@
 const TakePhotoBtn = document.getElementById("TakePhotoBtn");
 const FilePickerInput = document.getElementById("FilePickerInput");
+FilePickerInput.addEventListener("input", FilePicked);
 
 TakePhotoBtn.addEventListener("click", PopFilePicker);
 
 function PopFilePicker() {
     FilePickerInput.click();
 }
+
+function FilePicked() {
+    const Files = FilePickerInput.files;
+
+
+    if (Files.length > 0) {
+        let FileURL = window.URL.createObjectURL(Files[0]);
+        RecognizeText(FileURL, TextRecognized);
+    }
+}
+
+const model = await tf.loadLayersModel("../models/model.json");
